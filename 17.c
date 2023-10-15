1d9903fbba3e615a579f87903f0bd3381a4ac0dd
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-15 18:14:17 -0500

    Inicail ejercicio 17

--- 17.c
+++ 17.c
@@ -1,3 +1,9 @@
 /*Promediar los x primeros múltiplos de 2 y determinar si ese promedio 
 es mayor que los y primeros múltiplos de 5 para valores de x e y leídos.*/
 //Mateo Cisneros
+#include<stdio.h>
+int main()
+{
+    /* code */
+    return 0;
+}
