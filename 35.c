3aba0fa550990ee6e7f3c9fde054703eda7b7edf
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-15 17:58:02 -0500

    Inicial ejercicio 35

---
+++ 35.c
@@ -0,0 +1,9 @@
+/*Leer dos números enteros y determinar a cuánto es igual el producto 
+mutuo del primer dígito de cada uno.*/
+//Mateo Cisneros
+#include<stdio.h>
+int main()
+{
+    /* code */
+    return 0;
+}
