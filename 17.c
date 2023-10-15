656b36640d95f97f6a755bfcfea49c4a4c63508b
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-15 18:15:02 -0500

    Definir variables 17

--- 17.c
+++ 17.c
@@ -4,6 +4,13 @@ es mayor que los y primeros múltiplos de 5 para valores de x e y leídos.*/
 #include<stdio.h>
 int main()
 {
-    /* code */
+    int x, y;
+    int suma_multiplos_2 = 0;
+    int suma_multiplos_5 = 0;
+    int cantidad_multiplos_2 = 0;
+    int cantidad_multiplos_5 = 0;
+    float promedio_multiplos_2;
+    float promedio_multiplos_5;
+    
     return 0;
 }
